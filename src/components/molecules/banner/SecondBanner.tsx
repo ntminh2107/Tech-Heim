2ef73856731ade45db49c7b867595aaf9102d71d
datnt2002 <trongdat211002@gmail.com>
2024-06-19 11:34:57 +0700

    second banner

--- src/components/molecules/banner/SecondBanner.tsx
+++ src/components/molecules/banner/SecondBanner.tsx
@@ -1,10 +1,12 @@
 import { Button } from "antd";
 import ipImg from "../../../assets/images/banner2ip.png";
+import psImg from "../../../assets/images/ps53.png";
 
 const SecondBanner = () => {
   return (
-    <section className="flex mx-24 h-[420px] rounded-lg gap-6 mt-12">
-      <div className="basis-7/12 flex bg-gradient-to-br from-[#1975B9] via-[#1FB6CF] to-[#B0E9C9]">
+    <section className="flex mx-24 h-[420px] gap-6 mt-12">
+      {/* banner 1 */}
+      <div className="basis-7/12 rounded-lg flex bg-gradient-to-br from-[#1975B9] via-[#1FB6CF] to-[#B0E9C9]">
         <div className="basis-7/12">
           <h4 className="ml-12 mt-6 text-2xl text-black font-inter">
             Iphone <span className="text-white">15 Series</span>
@@ -47,7 +49,28 @@ const SecondBanner = () => {
           </Button>
         </div>
       </div>
-      <div className="basis-5/12">ef</div>
+      {/* banner 2 */}
+      <div className="relative basis-5/12 bg-[#005690] rounded-lg">
+        <div className="absolute bg-[#FCC870] h-24 w-24 rounded-br-full"></div>
+        <div className="absolute bg-[#FCC870] h-3/5 w-full bottom-0 rounded-t-[50%]"></div>
+        <h4 className="text-[#FCC870] text-2xl font-inter mt-8 text-center ">
+          Play Station 5
+        </h4>
+        <div className="absolute right-2 bottom-24">
+          <img src={psImg} alt="" className="object-contain" />
+        </div>
+        <div className="absolute bottom-0">
+          <h5 className="font-semibold text-xl font-inter text-[#005690] ml-11 mb-14">
+            Digital Edition + 2TB
+          </h5>
+          <Button
+            size="large"
+            className=" text-white bg-primary border-none  ml-16 mb-9"
+          >
+            Buy Now
+          </Button>
+        </div>
+      </div>
     </section>
   );
 };
