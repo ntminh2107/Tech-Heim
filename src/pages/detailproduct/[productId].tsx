becb458a4c066701e4ce3ad7996554bfe6fdd404
TowfMinh <vinxen2019@gmail.com>
2024-07-11 09:20:49 +0700

    done product detail

--- src/pages/detailproduct/[productId].tsx
+++ src/pages/detailproduct/[productId].tsx
@@ -30,7 +30,7 @@ const DetailProduct = () => {
   console.log(detailProduct);
 
   return (
-    <div className="flex flex-col gap-8">
+    <div className="flex flex-col gap-8 mb-14">
       <div className="flex gap-8 mb-8">
         <div className="flex gap-6">
           <div className="flex-shrink w-full">
