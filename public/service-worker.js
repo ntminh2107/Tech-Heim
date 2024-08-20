1f86bef8e368842b5649141be7edfdb5b633ebb5
TowfMinh <vinxen2019@gmail.com>
2024-08-20 09:49:56 +0700

    update fetching url

--- public/service-worker.js
+++ public/service-worker.js
@@ -21,7 +21,9 @@ let latestId = null;
 const setPooler = async () => {
   const interval = setInterval(async () => {
     console.log("[service worker] pooling notification...");
-    const res = await fetch(`http://localhost:3000/notification?_sort=-date`);
+    const res = await fetch(
+      `https://json-server-black-phi.vercel.app/notification?_sort=-date`
+    );
     console.log(res.status);
     if (res.ok) {
       const data = await res.json();
